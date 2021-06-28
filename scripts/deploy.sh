321f6aff31038046e15dc2a372a449008716829c
Marshall3572 <944797458@qq.com>
2021-06-28 16:59:29 +0800

    set pagewidth & pageHeight

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@github.com:FrankFang/Se1uqKnmKDZS.git &&
+git remote add origin git@github.com:Marshall3572/Data-visualization.git &&
 git push -uf origin master:gh-pages &&
 cd -;
