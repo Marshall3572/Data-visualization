09d60ea45000457a85e426cada1ee0714184f681
Marshall3572 <944797458@qq.com>
2021-07-07 15:09:11 +0800

    完成Chart7

--- src/components/chart-7.tsx
+++ src/components/chart-7.tsx
@@ -1,15 +1,54 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as echarts from 'echarts';
+import { createEchartsOptions } from '../shared/create-echarts-options';
+import { px } from '../shared/px';
 
 export const Chart7 = () => {
-    const divRef = useRef(null);
-    useEffect(() => {
-      var myChart = echarts.init(divRef.current);
-    }, []);
-  
-    return (
-      <div className="年龄段-图1">7
-        <div ref={divRef} className="chart" />
+  const divRef = useRef(null);
+  useEffect(() => {
+    var myChart = echarts.init(divRef.current);
+    myChart.setOption(createEchartsOptions({
+      color: ['#8D70F8', '#33A4FA'],
+      xAxis: { show: false },
+      yAxis: { show: false },
+      // 不好布局，已用两个span替代
+      legend: { show: false },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: ['75%', '90%'],
+          avoidLabelOverlap: false,
+          label: {
+            show: true, position: 'inside', textStyle: { color: 'white', fontSize: px(20) },
+            formatter(options) {
+              return options.value * 100 + '%';
+            }
+          },
+          labelLine: { show: false },
+          itemStyle: {
+            borderColor: '#0F113A',
+            borderWidth: px(4)
+          },
+          data: [
+            { value: 0.2, name: '女' },
+            { value: 0.8, name: '男' },
+          ]
+        }
+      ]
+    }));
+  }, []);
+
+  return (
+    <div className="年龄段-图1">7
+      <div className="chart">
+        <div className="main" ref={divRef}/>
+        <div className="text">性别</div>
       </div>
-    );
-  };
+      <div className="legend">
+        <span className="male"/>男
+        <span className="female"/>女
+      </div>
+    </div>
+  );
+};
