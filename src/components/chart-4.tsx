b7b1dc0a7a207688ac730e1bff051eba9e0237c7
Marshall3572 <944797458@qq.com>
2021-07-07 15:32:46 +0800

    完成Chart9

--- src/components/chart-4.tsx
+++ src/components/chart-4.tsx
@@ -26,7 +26,6 @@ export const Chart4 = () => {
             }
           },
           series: [{
-            name: '故意伤人',
             type: 'line',
             data: [
               0.15, 0.13, 0.11,
